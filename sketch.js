169ac0db44bbf7e18041e0bdf66443fc59699956
Alma-Co <edith.cantegrit@gmail.com>
2023-06-28 00:16:20 +0200

    travel portal concept uh

--- sketch.js
+++ sketch.js
@@ -1,10 +1,11 @@
 let y = 150;
 const totalTriangles = y*2;
 
+
 function setup() {
   createCanvas(720, 400);
   stroke(4, 55, 242);
-  frameRate(30);
+  frameRate(50);
 }
 
 function draw() {
@@ -13,8 +14,23 @@ function draw() {
   if (y < -totalTriangles) {
     y = height;
   }
-  fill(0);
+
   for (let i = 0; i < totalTriangles; i +=5) {
+    fillEvenTriangle(i)
     triangle(y + i, y + i, y + i, 60, 60, 60 + y + i);
+  }
+}
+
+function fillEvenTriangle(item) {
+  let black = color(0);
+  if(item === totalTriangles - 5){
+    fill(black);
+  }
+  else if(item === totalTriangles % 2 == 0){
+    let h = random(0, 242);
+    fill(4, 55, h);
+  }
+  else {
+    fill(black);
   }
 }
